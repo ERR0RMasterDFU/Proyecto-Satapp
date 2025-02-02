795f62abdbe78a0bfe186e60568d4c991af02450
Daniel Martinez <daniel1martinezleon@gmail.com>
2025-02-02 17:21:00 +0100

    a ver si es suficiente

--- proyecto_satapp/src/main/java/com/salesianostriana/dam/proyecto_satapp/services/PersonalService.java
+++ proyecto_satapp/src/main/java/com/salesianostriana/dam/proyecto_satapp/services/PersonalService.java
@@ -1,4 +1,57 @@
 package com.salesianostriana.dam.proyecto_satapp.services;
 
+import com.salesianostriana.dam.proyecto_satapp.models.Personal;
+import com.salesianostriana.dam.proyecto_satapp.repositories.PersonalRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class PersonalService {
+
+    private final PersonalRepository personalRepository;
+
+    public List<Personal> findAll() {
+        List<Personal> personals = personalRepository.findAll();
+
+        if (personals.isEmpty()) {
+            throw new EntityNotFoundException("No se encontró personal");
+        }
+        return personals;
+    }
+
+    public Personal findById(Long id) {
+        Optional<Personal> personal = personalRepository.findById(id);
+
+        if (personal.isPresent()) {
+            return personal.get();
+        } else {
+            throw new EntityNotFoundException("No se encontró personal");
+        }
+    }
+
+    public Personal save(Personal personal) {
+        personal.setRole("Personal");
+        return personalRepository.save(personal);
+    }
+
+    public Personal edit(Personal personal, Long id) {
+        return personalRepository.findById(id)
+                .map(old -> {
+                    old.setNombre(personal.getNombre());
+                    old.setUsername(personal.getUsername());
+                    old.setEmail(personal.getEmail());
+                    old.setPassword(personal.getPassword());
+                    return personalRepository.save(old);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("No se encontró personal"));
+    }
+
+    public void delete(Long id) {
+        personalRepository.deleteById(id);
+    }
 }
